b336ffc2c1d736c74ee79d8514315e89b7ca3864
Rammina <AltinaSchwarzer@gmail.com>
2020-12-21 07:27:21 +0800

    building frontend react application

--- src/config.js
+++ src/config.js
@@ -13,6 +13,9 @@ const config = {
     APP_CLIENT_ID: "36o49ge8isdgtdlrjs1e9iip16",
     IDENTITY_POOL_ID: "us-east-1:860df5ce-ecb9-459f-a9c9-e36ecc0ce6a4",
   },
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY:
+    "pk_test_51HuvlhH0GxnacGIVcDo6nNFJyrqaIWfmAdDWA7xn1vQHA42Lac4pbCCo5DSxUQPYabhx8hzYs3K1u5ahkr9crWER00DOtt9w9C",
 };
 
 export default config;
